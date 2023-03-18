4cd87549e0da11407336e6bb9598e48a09e77626
Krisztian <krisztian.valyi1@gmail.com>
2023-03-18 13:57:41 +0000

    Created database connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+//Create db connection
+mongoose.connect(`${process.env.DATABASE_URL}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+//logs error if connection fails
+db.on("error", (err) => {
+  console.log(err);
+});
+//logs message if connection is successful
+db.once("open", () => {
+  console.log("Connected to database.");
+});
 
 const app = express();
-dotenv.config();
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4000;
 
+//Create connection to server
 const server = app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
