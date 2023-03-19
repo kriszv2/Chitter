6cc1385f3105a53067c8bb07363bd9987bfbd56b
Krisztian <krisztian.valyi1@gmail.com>
2023-03-19 13:20:40 +0000

    can add/view posts

--- client/src/components/NewsPage/NewsPage.jsx
+++ client/src/components/NewsPage/NewsPage.jsx
@@ -6,17 +6,22 @@ export default function NewsPage(props) {
 
   console.log(props.posts)
   const posts = props.posts.map((post) => {
-    return post.message;
+
+    const { _id, message,createdAt } = post;
+    let formatTime = new Date(createdAt);
+    return (
+      <article key={_id} className='headlines-article'>
+        <p>{message}</p>
+        <time>{ formatTime.toLocaleString() }</time>
+    </article>
+    )
   })
   return (
     <>
         <main>
         <div className='div-container'>
-          
-          <p>
             {posts}
-          </p>
-          <PostOnWall/>
+          <PostOnWall posts={ posts} />
               </div> 
         </main>    
     </>
--- client/src/components/NewsPage/PostOnWall/PostOnWall.jsx
+++ client/src/components/NewsPage/PostOnWall/PostOnWall.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {  useState } from 'react'
 import "./PostOnWall.css"
+import axios from "axios"
 
-export default function PostOnWall() {
+export default function PostOnWall(props) {
+  const [postContent, setPostContent] = useState(``)
+  const [postsOnWall, setPostsOnWall] = useState([])
+
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post("http://localhost:4000", { message : postContent })
+      .then((response) => {
+        console.log("Success:", response.data);
+        
+        // handle successful response from server
+        
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // handle error
+      });
+
+  }
+  const changeHandler = (event) => {
+    setPostContent(event.target.value)
+  }
+  
   return (
-      <div>
-          <form method='POST' action='/'>
-              <input type="text" name="messageBox" id="PostMessageBox" />
-              <input type="button" value="Post message" />
+    <div>
+      
+          <form onSubmit={handleSubmit}>
+        <input type="text" name="messageBox" onChange={ changeHandler } value={postContent} id="PostMessageBox" />
+              <input type="submit" value="Post message" />
           </form>
     </div>
   )
