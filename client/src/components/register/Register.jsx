12fc4932284f9751d48994b4f0d1c69c76fd20c4
Krisztian <krisztian.valyi1@gmail.com>
2023-03-21 11:09:59 +0000

    Created some extra fields for register

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -3,13 +3,22 @@ import React from 'react'
 export default function Register() {
   return (
     <div>
-          <form>
+      <form>
+        <label>First name:
+              <input type="text"/>
+        </label>
+        <label>Last name:
+              <input type="text"/>
+        </label>
+        <label>email:
+              <input type="email"/>
+          </label>
             <label>Username:
               <input type="text"/>
           </label>
               
               <label>Password:
-                <input type="text" />
+                <input type="password" />
               </label>
               <input type="submit" value="Register" />
           </form>
